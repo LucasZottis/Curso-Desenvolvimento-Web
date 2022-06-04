01b0f434ab8372733373a1019c0787f5f9d81f97
FuturoProjeto <futuro.projeto@hotmail.com>
2022-06-04 12:29:23 -0300

    Aula 160 If / Else Parte 2 JavaScript

---
+++ JS/IfElse.js
@@ -0,0 +1,7 @@
+var conteudoAula = document.getElementById("ConteudoAula")
+
+if (2 === '2') {
+    conteudoAula.innerText = "Entrou no IF"
+} else {
+    conteudoAula.innerText = "Entrou no ELSE"
+}

ddad40bc902ab27b108cb0c51008023e18dc6cb0
FuturoProjeto <futuro.projeto@hotmail.com>
2022-06-04 19:29:53 -0300

    Aula 164 If / Else - Parte 3 JavaScript

--- JS/IfElse.js
+++ JS/IfElse.js
@@ -1,7 +1,36 @@
 var conteudoAula = document.getElementById("ConteudoAula")
 
-if (2 === '2') {
-    conteudoAula.innerText = "Entrou no IF"
+// if (2 === '2') {
+//     conteudoAula.innerText = "Entrou no IF"
+// } else {
+//     conteudoAula.innerText = "Entrou no ELSE"
+// }
+
+// if (2 == 2 && 3 >= 1) {
+//     conteudoAula.innerText += "Verdadeiro\n"
+// } else {
+//     conteudoAula.innerText += "Falso\n"
+// }
+
+// if (2 != 2 && 3 >= 1) {
+//     conteudoAula.innerText += "Verdadeiro\n"
+// } else {
+//     conteudoAula.innerText += "Falso\n"
+// }
+
+// if (!(2 != 2 && 3 >= 1)) {
+//     conteudoAula.innerText += "Verdadeiro\n"
+// } else {
+//     conteudoAula.innerText += "Falso\n"
+// }
+
+var nota = prompt("Digite a nota do aluno")
+var faltas = prompt("Digite a quantidade de faltas do aluno")
+var media = 7
+var faltasMaximas = 15
+
+if (nota >= media && faltas <= faltasMaximas) {
+    conteudoAula.innerText += "Aprovado\n"
 } else {
-    conteudoAula.innerText = "Entrou no ELSE"
+    conteudoAula.innerText += "Reprovado\n"
 }

df80d23ea40e2c86d1be23b6ac501e4dbce0a6ef
FuturoProjeto <futuro.projeto@hotmail.com>
2022-06-05 15:20:34 -0300

    Aula 180 Funções nativas para manipular strings JavaScript

--- JS/FuncoesGerais.js
+++ JS/FuncoesGerais.js
@@ -1,7 +1,13 @@
+var conteudoAula = document.getElementById("ConteudoAula")
+
 function EscreverTextoEmElemento(elemento, valor) {
     elemento.innerText += valor
 }
 
 function EscreverHtmlEmElemento(elemento, valor) {
     elemento.innerHTML += valor
+}
+
+function PularLinha(elemento) {
+    EscreverHtmlEmElemento(elemento, "<br/>")
 }

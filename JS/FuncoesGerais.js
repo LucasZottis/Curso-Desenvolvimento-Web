65f14c56ede91219f267652030a1427774282b61
FuturoProjeto <futuro.projeto@hotmail.com>
2022-06-05 17:00:44 -0300

    Aula 182 Funções nativas para manipular datas JavaScript

--- JS/FuncoesGerais.js
+++ JS/FuncoesGerais.js
@@ -1,24 +1,24 @@
 var conteudoAula = document.getElementById("ConteudoAula")
 
-function EscreverTextoEmElemento(valor) {
-    EscreverTextoEmElemento(conteudoAula, valor)
-}
+// function EscreverTextoEmElemento(valor) {
+//     EscreverTextoEmElemento(conteudoAula, valor)
+// }
 
 function EscreverTextoEmElemento(elemento, valor) {
     elemento.innerText += valor
 }
 
-function EscreverHtmlEmElemento(valor) {
-    EscreverHtmlEmElemento(conteudoAula, valor)
-}
+// function EscreverHtmlEmElemento(valor) {
+//     EscreverHtmlEmElemento(conteudoAula, valor)
+// }
 
 function EscreverHtmlEmElemento(elemento, valor) {
     elemento.innerHTML += valor
 }
 
-function PularLinha() {
-    PularLinha(conteudoAula)
-}
+// function PularLinha() {
+//     PularLinha(conteudoAula)
+// }
 
 function PularLinha(elemento) {
     EscreverHtmlEmElemento(elemento, "<br/>")
